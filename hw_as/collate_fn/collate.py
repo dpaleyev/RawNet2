83778f883adc186bc36df64cdd6532083416cb7b
Daniel Paleyev <danny.paleyev@gmail.com>
2023-12-15 02:39:32 +0300

    collate fix

--- hw_as/collate_fn/collate.py
+++ hw_as/collate_fn/collate.py
@@ -2,7 +2,7 @@ import torch
 import typing as tp
 
 def collate_fn(batch: tp.List[dict]):
-    audios = torch.cat([item['audio'][0] for item in batch], dim=0)
+    audios = torch.stack([item['audio'][0] for item in batch])
     lables = torch.LongTensor([item['label'] for item in batch])
     return {
         'audio': audios,
